e96cea3f54c2ebd38189e8e27866ab2ee757aa8c
Fulano <detias@outlook.com>
2021-04-01 15:20:39 -0300

    Lendo Arquivos de Texto

--- csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
+++ csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ByteBankImportacaoExportacao.Modelos;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,39 @@ namespace ByteBankImportacaoExportacao
     { 
         static void Main(string[] args) 
         {
+            var enderecoArquivo = @"..\..\contas.txt";
+
+            var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.Open);
+
+            //É um array que guarda as informações de Bytes temporarias 
+            var buffer = new byte[1024]; //1024 = 1 KB
+            var numeroDeBytesLidos = -1;
+
+            //Ele ta conseguindo ler um arq de 24kbts utilizando apenas um array de 1kbts
+            while(numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
             Console.ReadLine();
+        }
+
+        public static void EscreverBuffer(byte[] buffer)
+        {
+            var utf8 = new UTF8Encoding();
+           // var utf8 = Encoding.Default;
+
+            var texto = utf8.GetString(buffer);
+            Console.Write(texto);
+
+
+            //foreach (var myBite in buffer)
+            //{
+            //    Console.Write(myBite);
+            //    Console.Write(" ");
+            //}
+
         }
     }
 } 
